07c28238e2bc6a1546fb1aaf2c7ddbc3a3623a18
f0cuS <f0cuSmcsza@gmail.com>
2019-08-30 02:25:49 +0300

    Added conversation with a bot

---
+++ WPF_ChatBOT/ChatBot/ChatBotModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WPF_ChatBOT.Base;
+
+namespace WPF_ChatBOT.ChatBot
+{
+    class ChatBotModel
+    {
+        XmlSerializableDictionary<string, List<string>> answers;
+
+        public string imagePathBot { get; set; }
+
+        public ChatBotModel()
+        {
+            answers = new XmlSerializableDictionary<string, List<string>>();
+            loadSimpleData();
+            imagePathBot = @"C:\Users\User\Source\Repos\WPF_ChatBOT_MVVM\WPF_ChatBOT\WPF_ChatBOT\Images\photo.ico";
+        }
+
+
+        #region Singleton
+        private static ChatBotModel instance;
+
+        public static ChatBotModel getInstance()
+        {
+            if (instance == null)
+                instance = new ChatBotModel();
+            return instance;
+        }
+        #endregion
+
+        public void loadSimpleData()
+        {
+            this.answers.Add("Привет", new List<string>() { "Привет, как дела ?", "Здрарова, ты как там ?" });
+            this.answers.Add("Пока", new List<string>() { "До встречи!", "Пока, был рад встречи" });
+        }
+
+        public ObservableCollection<string> getAnswers(string key)
+        {
+            ObservableCollection<string> Answers = new ObservableCollection<string>();
+            foreach (KeyValuePair<string, List<string>> kv in this.answers)
+            {
+                if (kv.Key == key)
+                {
+                    foreach (var s in kv.Value)
+                        Answers.Add(s);
+                };
+            }
+
+            return Answers;
+        }
+
+        public ObservableCollection<string> getKeyWords()
+        {
+            ObservableCollection<string> keyWords = new ObservableCollection<string>();
+            foreach (KeyValuePair<string, List<string>> kv in this.answers)
+            {
+                keyWords.Add(kv.Key);
+            }
+            return keyWords;
+        }
+
+
+        #region Serializable
+        public void LoadAnswers()
+        {
+
+        }
+
+        public void SaveAnswers()
+        {
+
+        }
+        #endregion
+
+
+    }
+}
---
+++ WPF_ChatBOT/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using WPF_ChatBOT;
+using static WPF_ChatBOT.Base.BaseViewModel;
+
+namespace WPF_ChatBOT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.ResizeMode = ResizeMode.CanResize;
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void btnFull_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void btnRollDown_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            WPF_ChatBOT.ChatBot.ChatBotView cbv = new ChatBot.ChatBotView();
+            cbv.ShowDialog();
+        }
+
+
+
+
+
+
+    }
+}
+
